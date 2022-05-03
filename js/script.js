dbaf0b62cfa8df23cea7cc0ef82bfa407ff3a29f
RAWx000 <97886563+RAWx000@users.noreply.github.com>
2022-05-04 03:12:48 +1000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,62 +21,95 @@ $(document).ready(function() {
     showScroll('.advantages__wrap');
 
     function scaleOn(element, element1, item) {
-        $(element).mouseover(function() {  
+        $(element1).mouseover(function() {
+            $(".advantages__sun_player").addClass("shake_that");
             $(element1).removeClass("scale_off");    
             $(element1).addClass("scale_on");
+            $(element).addClass("shine_on");
             if (item == 1) {
-                $(".bokeh_advantages").stop(true).animate({left: "-297px", bottom: "-170px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point1_img").css('opacity', '0.2');
             }
             else if (item == 2) {
-                $(".bokeh_advantages").stop(true).animate({left: "-734px", bottom: "-379px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point2_img").css('opacity', '0.2');
             }
             else if (item == 3) {
-                $(".bokeh_advantages").stop(true).animate({left: "-734px", bottom: "-561px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point4_img").css('opacity', '0.2');
             }
             else if (item == 4) {
-                $(".bokeh_advantages").stop(true).animate({left: "-710px", bottom: "-717px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point6_img").css('opacity', '0.2');
             }
             else if (item == 5) {
-                $(".bokeh_advantages").stop(true).animate({left: "-297px", bottom: "-873px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point8_img").css('opacity', '0.2');
             }
             else if (item == 6) {
-                $(".bokeh_advantages").stop(true).animate({left: "70px", bottom: "-750px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point7_img").css('opacity', '0.2');
             }
             else if (item == 7) {
-                $(".bokeh_advantages").stop(true).animate({left: "115px", bottom: "-557px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point5_img").css('opacity', '0.2');
             }
             else if (item == 8) {
-                $(".bokeh_advantages").stop(true).animate({left: "120px", bottom: "-360px", queue: false, opacity: 0.35}, 300);
                 $(".advantages__items_point3_img").css('opacity', '0.2');
+            }
+            $(".advantages__items_raycast_"+item).css('opacity', '0.7');
+        });
+
+        $(element).mouseover(function() {
+            $(".advantages__sun_player").addClass("shake_that");
+            $(element1).removeClass("scale_off");    
+            $(element1).addClass("scale_on");
+            $(element).addClass("shine_on");
+            if (item == 1) {
+                $(".advantages__items_point1_img").css('opacity', '0.2');
+            }
+            else if (item == 2) {
+                $(".advantages__items_point2_img").css('opacity', '0.2');
+            }
+            else if (item == 3) {
+                $(".advantages__items_point4_img").css('opacity', '0.2');
+            }
+            else if (item == 4) {
+                $(".advantages__items_point6_img").css('opacity', '0.2');
+            }
+            else if (item == 5) {
+                $(".advantages__items_point8_img").css('opacity', '0.2');
+            }
+            else if (item == 6) {
+                $(".advantages__items_point7_img").css('opacity', '0.2');
+            }
+            else if (item == 7) {
+                $(".advantages__items_point5_img").css('opacity', '0.2');
+            }
+            else if (item == 8) {
+                $(".advantages__items_point3_img").css('opacity', '0.2');
+            }
+            $(".advantages__items_raycast_"+item).css('opacity', '0.7');
+        });
+
+        $(element).mouseleave(function() {  
+            $(".advantages__sun_player").removeClass("shake_that");
+            $(element1).removeClass("scale_on");    
+            $(element1).addClass("scale_off");
+            $(element).removeClass("shine_on");
+            for ( let i = 1; i < 9; i++ ) {
+                $(".advantages__items_point"+i+"_img").css('opacity', '1');
+                $(".advantages__items_raycast_"+i).css('opacity', '0');
             }
         });
         
-        $(element).mouseleave(function() {    
+        $(element1).mouseleave(function() {    
+            $(".advantages__sun_player").removeClass("shake_that");
             $(element1).removeClass("scale_on");  
             $(element1).addClass("scale_off");
-            $(".bokeh_advantages").stop(true).animate({left: "-297px", bottom: "-510px", queue: false, opacity: 0.2}, 300);
+            $(element).removeClass("shine_on");
             for ( let i = 1; i < 9; i++ ) {
                 $(".advantages__items_point"+i+"_img").css('opacity', '1');
+                $(".advantages__items_raycast_"+i).css('opacity', '0');
             }
         });
+        
     }
     for ( let i = 1; i < 9; i++ ) {
         scaleOn('.advantages__items_sunshine_'+i, '.advantages__items_point_text_'+i, i);
     }
-
-    const video = document.querySelectorAll('video');
-
-    $(".advantages__button").click(function() {
-        video[0].play();
-        $(".advantages__button").fadeOut('fast').delay(25000).fadeIn();
-    });
     
-  
 });
